a2e06cd1502effe6aeac7c3721ba392d5cdc24b5
Mohammad Chohan <103134889@student.swin.edu.au>
2022-11-27 08:36:15 +1100

    Taking Pictures with Intents

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -3,7 +3,9 @@ package com.bignerdranch.android.criminalintent
 import android.content.Context
 import androidx.room.Room
 import com.bignerdranch.android.criminalintent.database.CrimeDatabase
-import com.bignerdranch.android.criminalintent.database.migration_1_2
+import com.bignerdranch.android.criminalintent.database.MIGRATION_1_2
+import com.bignerdranch.android.criminalintent.database.MIGRATION_2_4
+import com.bignerdranch.android.criminalintent.database.MIGRATION_3_2
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
@@ -22,7 +24,7 @@ class CrimeRepository private constructor(
             CrimeDatabase::class.java,
             DATABASE_NAME
         )
-        .addMigrations(migration_1_2)
+        .addMigrations(MIGRATION_1_2, MIGRATION_3_2, MIGRATION_2_4)
         .build()
 
     fun getCrimes(): Flow<List<Crime>> = database.crimeDao().getCrimes()
--- app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDatabase.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDatabase.kt
@@ -7,16 +7,41 @@ import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.bignerdranch.android.criminalintent.Crime
 
-@Database(entities = [Crime::class], version = 2, exportSchema = false)
+@Database(entities = [Crime::class], version = 4, exportSchema = false)
 @TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase : RoomDatabase() {
     abstract fun crimeDao(): CrimeDao
 }
 
-val migration_1_2 = object : Migration(1, 2) {
+val MIGRATION_1_2 = object : Migration(1, 2) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL(
             "ALTER TABLE Crime ADD COLUMN suspect TEXT NOT NULL DEFAULT ''"
+        )
+    }
+}
+
+val MIGRATION_3_2 = object : Migration(3, 2) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL(
+            "ALTER TABLE Crime RENAME TO oldCrime"
+        )
+        database.execSQL(
+            "CREATE TABLE Crime (id BLOB NOT NULL, title TEXT NOT NULL, date INTEGER NOT NULL, isSolved INTEGER NOT NULL, suspect TEXT NOT NULL, PRIMARY KEY(id)) "
+        )
+        database.execSQL(
+            "INSERT INTO Crime (id, title, date, isSolved, suspect) SELECT id, title, date, isSolved, suspect FROM oldCrime"
+        )
+        database.execSQL(
+            "DROP TABLE oldCrime"
+        )
+    }
+}
+
+val MIGRATION_2_4 = object : Migration(2, 4) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL(
+            "ALTER TABLE Crime ADD COLUMN photoFileName Text"
         )
     }
 }
